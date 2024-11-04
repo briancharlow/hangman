206820282f72f39b2e3a0e94995c57d783e5c42e
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-11-04 22:48:22 +0300

    creates the hangman drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import { useState } from "react"
+import words from "./wordList.json"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { Keyboard } from "./Keyboard"
+import { HangmanWord } from "./HangmanWord"
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("test")
+  const [wordToGuess, setWordToGuess] = useState(()=>{
+      return words[Math.floor(Math.random() * words.length)]
+  })
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
   return (
-    <h1> welcome</h1>
+   <div
+   style={{
+    maxWidth: "800px",
+    display: "flex",
+    flexDirection: "column",
+    gap:"2rem",
+    margin: "0 auto",
+    alignItems: "center"
+   }}>
+    <div style={{ fontSize: "2rem", textAlign: "center" }}> Lose Win </div>
+   <div>
+    <HangmanDrawing/>
+    <HangmanWord/>
+    <Keyboard/>
+   </div>
+   </div>
   )
 }
 
